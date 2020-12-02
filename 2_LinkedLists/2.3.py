7fc7b5ca615be22309fece8568fe513b0385b87f
kristinamb15 <kleifest@gmail.com>
2020-12-02 16:29:54 -0700

    Added unit test to 2.3

--- 2_LinkedLists/2.3.py
+++ 2_LinkedLists/2.3.py
@@ -2,6 +2,8 @@
 # given only access to that node
 # Ex: delete c from a, b, c, d, e, f
 
+import unittest
+
 from linked_list import (Node, SLinkedList)
 
 # Solution 1:
@@ -14,6 +16,7 @@ def delete_mid(llist, data):
             current.next = current.next.next
             break
         current = current.next
+    return llist
 
 # Solution 2: without invoking the list at all
 # O(1)
@@ -24,25 +27,35 @@ def delete_mid_alt(el):
         el.data = el.next.data
         el.next = el.next.next
 
-example1 = SLinkedList()
-example1.add_multi(1, 5, 9, 12)
-delete_mid(example1, 9)
-example1.pretty_print()
+# Testing
+class Tests(unittest.TestCase):
 
-example2 = SLinkedList()
-example2.add_multi(1, 3, 4, 5, 6, 8, 3, 4)
-delete_mid(example2, 4)
-example2.pretty_print()
+    def test_delete_mid_single_occur(self):
+        llist = SLinkedList()
+        llist.add_multi(1, 5, 9, 12)
+        self.assertEqual(delete_mid(llist, 9).data_list(), [1,5,12])
 
-node1 = Node(1)
-node5 = Node(5)
-node9 = Node(9)
-node12 = Node(12)
+    def test_delete_mid_multi_occur(self):
+        llist = SLinkedList()
+        llist.add_multi(1, 3, 4, 5, 6, 8, 3, 4)
+        self.assertEqual(delete_mid(llist, 4).data_list(), [1,3,5,6,8,3,4])
+    
+    def test_delete_mid_false(self):
+        llist = SLinkedList()
+        llist.add_multi(1, 2)
+        self.assertEqual(delete_mid(llist, 3).data_list(), [1,2])
+    
+    def test_delete_mid_alt(self):
+        node1 = Node(1)
+        node5 = Node(5)
+        node9 = Node(9)
+        node12 = Node(12)
+        node1.next = node5
+        node5.next = node9
+        node9.next = node12
+        llist = SLinkedList(node1)
+        delete_mid_alt(node9)
+        self.assertEqual(llist.data_list(), [1,5,12])
 
-node1.next = node5
-node5.next = node9
-node9.next = node12
-example3 = SLinkedList(node1)
-example3.pretty_print()
-delete_mid_alt(node9)
-example3.pretty_print()
+if __name__ == '__main__':
+    unittest.main()
