c37823f6fca327b76516fab494f4469f72e5e308
kristinamb15 <kleifest@gmail.com>
2020-12-18 14:58:08 -0700

    Fixed 3.5 and added unit test

--- 3_StacksQueues/3.5.py
+++ 3_StacksQueues/3.5.py
@@ -1,62 +1,61 @@
 # 3.5 Sort Stack: Write a program to sort a stack such that the smallest items are on the top.
 # You can use an additional temporary stack, but you may not copy the elements into any other data structure.
-# The stack supports push, pop. peed, and isEmpty.
+# The stack supports push, pop, peek, and isEmpty.
 
+import unittest
 from queue import LifoQueue
+
+# LifoQueue has no built in peek method
+def peek_stack(stack):
+    item = stack.get()
+    stack.put(item)
+    return item
 
 # Solution 1:
 # O(n^2) ?
 def sort_stack(stack):
-    temp = LifoQueue()
-    run = True
-    while run:
-        min_item = stack.get()
-        while not stack.empty():
-            next_item = stack.get()
-            if min_item < next_item:
-                temp.put(next_item)
-            else:
-                temp.put(min_item)
-                min_item = next_item    
-        
-        temp_run = True
-        while not temp.empty() and temp_run:
-            # Would use peek here but it doesn't exist in LifoQueue
-            item = temp.get()
-            if item > min_item:
-                stack.put(item)
-            else:
-                temp.put(item)
-                temp_run = False
-        
-        temp.put(min_item)
+    temp_stack = LifoQueue()
 
-        if stack.empty():
-            run = False
-    
-    while not temp.empty():
-        item = temp.get()
-        stack.put(item)
+    while not stack.empty():
+        # get next element of stack
+        temp = stack.get()
+
+        # Push temp stack elements back into stack until we find the right place for the temp element
+        while not temp_stack.empty() and peek_stack(temp_stack) > temp:
+            stack.put(temp_stack.get())
+
+        # Put temp element in temp stack
+        temp_stack.put(temp)           
+
+    # Put elements back in stack    
+    while not temp_stack.empty():
+        stack.put(temp_stack.get())    
     
     return stack
 
-def printStack(stack):
+# Turns stack into list for testing/printing
+def list_stack(stack):
     printer = []
     while not stack.empty():
         temp = stack.get()
         printer.append(temp)
-    print(printer[::-1])
-    while len(printer) > 0:
-        temp = printer.pop()
-        stack.put(temp)
+    for item in printer[::-1]:
+        stack.put(item)
+    return printer[::-1]
 
-ex1 = LifoQueue()
-ex1.put(2)
-ex1.put(1)
-ex1.put(4)
-ex1.put(3)
-ex1.put(5)
-printStack(ex1)
+# Testing
+class Tests(unittest.TestCase):
 
-sort_stack(ex1)
-printStack(ex1)
+    def setUp(self):
+        self.my_stack = LifoQueue()
+        self.my_stack.put(4)
+        self.my_stack.put(1)
+        self.my_stack.put(2)
+        self.my_stack.put(5)
+        self.my_stack.put(3)
+
+    def test_sort_stack(self):
+        self.assertEqual(list_stack(sort_stack(self.my_stack)), [5, 4, 3, 2, 1])
+
+if __name__ == '__main__':
+    unittest.main()
