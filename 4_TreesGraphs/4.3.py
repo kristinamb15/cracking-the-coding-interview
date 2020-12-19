f615bee90923e253d9327a6e4ded686ca5cac308
Kristina Brantley <67759043+kristinamb15@users.noreply.github.com>
2020-12-19 13:08:55 -0700

    Delete 4.3.py

--- 4_TreesGraphs/4.3.py
+++
@@ -1,7 +0,0 @@
-# 4.3 List of Depths: Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth.
-
-import trees
-import linked_list
-
-def depth_list(tree):
-    
